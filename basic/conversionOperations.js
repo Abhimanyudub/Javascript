4a90ceed8677a7ef5171fa700e9e7d1c87b464cb
Abhimanyu Dub <dubabhimanyu@gmail.com>
2025-09-07 16:53:46 +0000

    Conversion Operation

---
+++ basic/conversionOperations.js
@@ -0,0 +1,34 @@
+
+let score1 = "33"
+let score = "Abhimanyu"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+console.log(typeof score1);
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+//  "33abc" => NaN
+// true => 1;false => 0;
+
+let isLoggedIn = "Abhimanyu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Abhimanyu" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
