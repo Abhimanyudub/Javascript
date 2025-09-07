067b0abf6e2ae9c54c6773d75919628c64acfb36
Abhimanyu Dub <dubabhimanyu@gmail.com>
2025-09-07 15:54:52 +0000

    first commit

---
+++ basic/variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345;
+let accountEmail = "asdf@gmail";
+var accountPassword = "1234"
+accountCity = "jaipur";
+
+
+// accountId = 2345; not allowed
+
+accountEmail = "cvbnm@gmail123";
+accountPassword = "6789";
+accountCity = "bangaluru";
+
+
+
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity]);
