78059dc4bf01d13d006cf510b3ad97452cec6a2c
Abhimanyu Dub <dubabhimanyu@gmail.com>
2025-09-07 16:27:55 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,28 @@
+"Use strict"//treat all js code as newer version
+  
+
+/* 
+console.log(3+
+    3); // code readability should be high
+    */
+
+let name = "Abhimanyu";
+let age = 21;
+let isLoggedIn = false;
+let state = null;
+let country;
+
+// number => 2 to power 53
+// bigInt
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+
+
+console.log(typeof null); //it is a type of object
+console.log(typeof undefined); // undefined
