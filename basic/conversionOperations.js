1d6560adc3934e7ad4e0af8a1d702fac6c2eb837
Abhimanyu Dub <dubabhimanyu@gmail.com>
2026-01-06 09:50:35 +0000

    operations

--- basic/conversionOperations.js
+++ basic/conversionOperations.js
@@ -31,4 +31,48 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+/*************************Operations******************************** */
+
+
+ let value = 3
+ let negValue = -value
+ console.log(negValue);
+ 
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**2);
+//  console.log(2/2);
+//  console.log(2%2);
+
+
+let str1 = "Abhimanyu";
+let str2 = "Hello";
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+console.log(true);
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
 
